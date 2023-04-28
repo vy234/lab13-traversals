1bbae1f97bec9d89184e96f719cc3cf80cc617ff
Akhil Madusudan <akhilmadusudan@gmail.com>
2023-04-28 14:57:33 -0700

    Updated Traversal Code

--- src/bst/BinarySearchTree.java
+++ src/bst/BinarySearchTree.java
@@ -116,8 +116,10 @@ public class BinarySearchTree<T extends Comparable<T>> {
 	//Traverse the tree in an preorder fashion
 	//Print the current node first and then recurse on the children
 	public void preOrder() {
+		
 		preOrderRecurse(root); 
-		System.out.println(“Pre0rder test commit”);
+		System.out.println("PreOrder test commit");
+		
 	}
 	
 	private void preOrderRecurse(BSTNode<T> node) {
@@ -138,6 +140,7 @@ public class BinarySearchTree<T extends Comparable<T>> {
 	//For a bst this will print the values in sorted order from smallest to largest
 	public void inOrder() {
 		inOrderRecurse(root); 
+		
 	}
 	
 	public void inOrderRecurse(BSTNode<T> node) {
@@ -154,6 +157,7 @@ public class BinarySearchTree<T extends Comparable<T>> {
 	//Recurse on the children and then print the value in the current node
 	public void postOrder() {
 		postOrderRecurse(root); 
+		System.out.println("PostOrder test commit");
 	}
 	
 	public void postOrderRecurse(BSTNode<T> node) {
